b869f26726b1d3dbd2f94275b8b72171cc21dbb4
Fannci <donot322bm@gmail.com>
2023-02-13 07:26:22 -0500

    new changes

--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -2,25 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import male_avatar from "../assets/images/undraw_male_avatar.svg";
 import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { usenameValidate } from "../helper/validate";
 
 const Username = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validate: usenameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className='container mx-auto'>
-      <div className='flex h-screen'>
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+      <div className='flex justify-center items-center h-screen'>
         <div className={styles.glass}>
           <div className='title flex flex-col items-center'>
             <h4 className='text-5xl font-bold'>Hello Again!</h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'>Explore More by connecting with us.</span>
           </div>
 
-          <form className='py-1' action=''>
+          <form className='py-1' onSubmit={formik.handleSubmit}>
             <div className='profile flex justify-center py-4'>
-              <img src={male_avatar} alt='avatar' />
+              <img className={styles.profile_img} src={male_avatar} alt='avatar' />
             </div>
 
             <div className='textbox flex flex-col items-center gap-6'>
-              <input type='text' placeholder='Username' />
-              <button type='submit'> Let's Go </button>
+              <input {...formik.getFieldProps("username")} className={styles.textbox} type='text' placeholder='Username' />
+              <button className={`${styles.btn} bg-indigo-500`} type='submit'>
+                {" "}
+                Let's Go{" "}
+              </button>
             </div>
 
             <div className='text-center py-4'>
